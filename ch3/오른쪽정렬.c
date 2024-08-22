375763964eb92f2ff4f29e66e71939a67db9c488
hyeon-i <starjea0126@gmail.com>
2024-08-22 17:48:37 +0900

    3장.조건대로반복

---
+++ ch3/오른쪽정렬.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - 1; j++) 
+		{
+			printf(" "); // printf("S");					
+
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}          
